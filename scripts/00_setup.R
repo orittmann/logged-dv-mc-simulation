14352e18a90581c26f454625698822f78e6df261
Oliver Rittmann <rittmann@mobile-148-228.wlan.uni-mannheim.de>
2020-02-19 11:34:18 +0100

    Hello world! First simulation setup

---
+++ scripts/00_setup.R
@@ -0,0 +1,15 @@
+# Simulating Quantities of Interest when the Dependent Variable is logged
+
+##### Setup
+
+## ----setup----
+## Save package names as a vector of strings
+pkgs <-
+  c("MASS",
+    "scales")
+
+## Install uninstalled packages
+lapply(pkgs[!(pkgs %in% installed.packages())], install.packages)
+
+## Load all packages to library and adjust options
+lapply(pkgs, library, character.only = TRUE)
---
+++ scripts/01_simulation_setup.R
@@ -0,0 +1,54 @@
+
+# population size
+m <- 10000
+
+# means
+mus <- c(5, 10)
+
+# SD
+sds <- c(4, 3)
+sds_mat <- diag(sds)
+
+# Correlation matrix (we assume some multicollinearity)
+cor_mat <- matrix(c(1, 0.2, 0.2, 1), nrow = 2, ncol = 2)
+
+# Convert to variance covariance matrix
+varcov <- sds_mat %*% cor_mat %*% sds_mat 
+
+# true coefficients
+b <- c(2, 0.3, -0.5)
+
+# Choose sigma.est
+sigma_est <- 1
+
+gen_pop <- function(m,          # population size
+                    mus,        # means
+                    varcov,     # variance-covariance matrix
+                    b,          # true coefficients
+                    sigma_est   # sigma est
+                    ){
+  X <- mvrnorm(n = m, mu = mus, Sigma = varcov) 
+  mu_y <- cbind(1, X) %*% b
+  Y <- exp(rnorm(m, mu_y, sigma_est))
+  population <- data.frame("Y" = Y,
+                           "X1" = X[,1],
+                           "X2" = X[,2])
+  return(population)
+}
+
+pop <- gen_pop(m = m,
+               mus = mus, 
+               varcov = varcov, 
+               b = b, 
+               sigma_est = sigma_est)
+
+dta <- pop[sample(1:m, size = 1000),]
+
+# Plot the data
+
+plot(dta$X1, log(dta$Y))
+plot(dta$X1, dta$Y)
+
+plot(dta$X2, log(dta$Y))
+plot(dta$X2, dta$Y)
+
