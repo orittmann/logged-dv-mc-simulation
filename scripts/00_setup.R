8371d4132c68ce90ea8e56afbcf3c17119e817f3
mneunhoe <mneunhoe@mail.uni-mannheim.de>
2020-02-21 17:26:18 +0100

    Playing around with simulations

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -13,3 +13,5 @@ lapply(pkgs[!(pkgs %in% installed.packages())], install.packages)
 
 ## Load all packages to library and adjust options
 print(sapply(pkgs, require, character.only = TRUE))
+
+rm(list = ls())
